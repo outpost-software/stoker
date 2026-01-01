8bf05beb76fcaaf787016399a5f3b8a71812ab1f
Casey Butler <32690095+bigjimhere@users.noreply.github.com>
2026-01-01 11:16:34 +1100

    docs: set up gtag

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -64,6 +64,10 @@ const config: Config = {
                 theme: {
                     customCss: "./src/css/custom.css",
                 },
+                gtag: {
+                    trackingID: "AW-1061135520",
+                    anonymizeIP: true,
+                },
             } satisfies Preset.Options,
         ],
     ],
