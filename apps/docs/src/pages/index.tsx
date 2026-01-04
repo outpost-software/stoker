34540a0c3cf6e400977ef801a2d2bf4de59697cf
Casey Butler <32690095+bigjimhere@users.noreply.github.com>
2026-01-04 12:34:14 +1100

    docs: update CTA

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
                 </div>
                 <div className={styles.buttons}>
                     <Link className="button button--secondary button--lg" to="/docs/Getting Started">
-                        Get Started
+                        Read the docs
                     </Link>
                 </div>
                 <p className={styles.free}>Free for development and evaluation*</p>
