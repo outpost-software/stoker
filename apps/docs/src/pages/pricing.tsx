bbc039d39f5ec743c2cfd41a98b9beb6d8a28fd1
Casey Butler <32690095+bigjimhere@users.noreply.github.com>
2026-02-09 17:45:14 +1100

    docs: update pricing page

--- apps/docs/src/pages/pricing.tsx
+++ apps/docs/src/pages/pricing.tsx
@@ -5,8 +5,9 @@ const PricingCard = ({ children, description, price, type, subscription, buttonT
     return (
         <>
             <div className="w-full px-4 md:w-1/2 lg:w-1/3">
-                <div className="relative min-h-[400px] z-10 mb-10 overflow-hidden rounded-[10px] border-2 border-solid border-stroke bg-white px-8 py-10 shadow-pricing dark:border-dark-3 dark:bg-dark-2 sm:p-12 lg:px-6 lg:py-10 xl:p-[50px]">
+                <div className="relative min-h-[450px] 2xl:min-h-[400px] z-10 mb-10 overflow-hidden rounded-[10px] border-2 border-solid border-stroke bg-white px-8 py-10 shadow-pricing dark:border-dark-3 dark:bg-dark-2 sm:p-12 lg:px-6 lg:py-10 xl:p-[50px]">
                     <span className="mb-3 block text-lg font-semibold text-primary">{type}</span>
+                    <br></br>
                     <h2 className="mb-5 text-[42px] font-bold text-dark dark:text-white">
                         {price}
                         <span className="text-base font-medium text-body-color dark:text-dark-6">{subscription}</span>
@@ -313,41 +314,28 @@ const Pricing = () => {
                     </div>
 
                     <div className="-mx-4 flex flex-wrap justify-center">
-                        <div className="-mx-4 flex flex-wrap">
-                            <PricingCard
-                                active={true}
-                                type="Development"
-                                price="Free"
-                                subscription=""
-                                description="Use Stoker for free while you develop your app.*"
-                                buttonText="Go to the docs"
-                                buttonLink="/docs/Getting Started"
-                            >
-                                <></>
-                            </PricingCard>
-                            <PricingCard
-                                active={false}
-                                type="First Production Tenant"
-                                price="USD $29"
-                                subscription="/ month"
-                                description="This applies to an app's first active production tenant."
-                                buttonText="Contact Us"
-                                buttonLink="/contact"
-                            >
-                                <></>
-                            </PricingCard>
-                            <PricingCard
-                                active={false}
-                                type="Additional Production Tenants"
-                                price="USD $9"
-                                subscription="/ month"
-                                description="Additional production tenants are free for the first 30 days. Contact us for bulk discounts."
-                                buttonText="Contact Us"
-                                buttonLink="/contact"
-                            >
-                                <></>
-                            </PricingCard>
-                        </div>
+                        <PricingCard
+                            active={true}
+                            type="Development"
+                            price="Free"
+                            subscription=""
+                            description="Use Stoker for free while you develop your app.*"
+                            buttonText="Go to the docs"
+                            buttonLink="/docs/Getting Started"
+                        >
+                            <></>
+                        </PricingCard>
+                        <PricingCard
+                            active={false}
+                            type="Production Tenants"
+                            price="USD $79"
+                            subscription="/ tenant / month"
+                            description="Production tenants are free for the first 30 days. Contact us for bulk discounts."
+                            buttonText="Contact Us"
+                            buttonLink="/contact"
+                        >
+                            <></>
+                        </PricingCard>
                     </div>
                 </div>
                 <p style={{ textAlign: "center", paddingLeft: "15%", paddingRight: "15%" }}>
